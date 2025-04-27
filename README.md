642b8dad50cc370b27552635d85d772ac1a50f3e
b33-stinger <b33-stinger@users.noreply.github.com>
2025-04-27 22:21:16 +0900

    v1.0.1

--- README.md
+++ README.md
@@ -7,24 +7,28 @@
 git clone https://github.com/b33-stinger/ramc.git
 cd ramc
 cargo build --release
-target/release/./ramc
-# OR #
-cargo run -- --help
 ```
 
 ### Start
 ```
 mv target/release/ramc ./
-./ramc
+./ramc --help
 ```
 
 
 ### Usage
 ```
-  -d, --download-url <DOWNLOAD_URL>  URL to get the Mirrors from        [default: https://archlinux.org/download/]
-  -l, --log-file <LOG_FILE>          log file name                      [default: log.json]
-  -c, --continous-log                dump to log after each iteration   [default: false]
-  -h, --help                         Print help
+  -d, --download-url <DOWNLOAD_URL>         URL to get the Mirrors from             [default: https://archlinux.org/download/]
+  -a, --ask-custom-url                      ask for a custom download URL (stdin)   [default: false]
+  -m, --max-check <MAX_CHECK>               Max Mirrors to check (-1 == all)        [default: -1]
+  -l, --log-file <LOG_FILE>                 log file name                           [default: log.json]
+  -c, --continous-log                       dump to log after each iteration        [default: false]
+  -e, --exclude-country <EXCLUDE_COUNTRY>   Exclude Country                         [default: None]
+  -i, --include-country <INCLUDE_COUNTRY>   Exclude Country                         [default: None]
+  -q, --quiet                               Don't log (stdout) data                 [default: false]
+  -n, --no-log                              Don't log (file) data                   [default: false]
+  -h, --help                                Print help
+
 ```
 
 
--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,36 @@ use std::io::{self, Write};
 use std::time::Duration;
 
 use once_cell::sync::Lazy;
+use text_io::read;
 
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long, help = "URL to get the Mirrors from", default_value_t = String::from("https://archlinux.org/download/"))]
     download_url: String,
 
-    //#[arg(short, long, default_value_t = false)]
-    //ask_custom_url: bool,
+    #[arg(short, long, default_value_t = false, help = "ask for a custom download URL (stdin)")]
+    ask_custom_url: bool,
 
-    //#[arg(short, long, default_value_t = 0)]
-    //max_check: usize,
+    #[arg(short, long, default_value_t = -1, help = "Max Mirrors to check (-1 == all)")]
+    max_check: isize,
 
     #[arg(short, long, help = "log file name", default_value_t =  String::from("log.json"))]
     log_file: String,
 
     #[arg(short, long, help = "dump to log after each iteration", default_value_t = false)]
     continous_log: bool,
+
+    #[arg(short, long, help = "Exclude Country", default_value_t = String::from(""))]
+    exclude_country: String,
+
+    #[arg(short, long, help = "Exclude Country", default_value_t = String::from(""))]
+    include_country: String,
+
+    #[arg(short, long, help = "Don't log (stdout) data", default_value_t = false)]
+    quiet: bool,
+
+    #[arg(short, long, help = "Don't log (file) data", default_value_t = false)]
+    no_log: bool,
 }
 /*
 DONE_MIRRORS[0].0 = Working     (amount)
@@ -50,7 +63,6 @@ LOG_DATA["some_url"]["status"]  = if 1 == Working else Not Working
 */
 static LOG_DATA: Lazy<Mutex<serde_json::Value>> = Lazy::new(|| { Mutex::new(json!({}))});
 
-
 fn log(data: Option<serde_json::Value>) -> u8 {
     let data = data.unwrap_or(json!({}));
     let args: Args = Args::parse();
@@ -70,7 +82,9 @@ fn log(data: Option<serde_json::Value>) -> u8 {
 }
 
 
-fn parse_mirrors(raw_html: String) -> Vec<Vec<String>> {
+fn parse_mirrors(raw_html: String, max_check: isize, country_exclude: Vec<&str>, country_include: Vec<&str>) -> Vec<Vec<String>> {
+    let mut check_count = 0;
+
     let document = Html::parse_document(&raw_html);
     
     let div_sel    = Selector::parse("div#download-mirrors").unwrap();
@@ -92,20 +106,35 @@ fn parse_mirrors(raw_html: String) -> Vec<Vec<String>> {
                 .to_string();
 
             for a in ul.select(&link_sel) {
+                let country = section.clone().to_lowercase();
+
+                if !country_include.is_empty() && !country_include.contains(&&country.as_str()){
+                    continue;
+                }
+
+                if !country_exclude.is_empty() && country_exclude.contains(&&country.as_str()){
+                    continue;
+                }
+
+                if check_count == max_check{
+                    return out;
+                }
+                check_count += 1;
+                
                 let url    = a.value().attr("href").unwrap_or("").to_string();
                 let domain = a.text().collect::<Vec<_>>().join(" ").trim().to_string();
-                out.push(vec![url, domain, section.clone()]);
+                out.push(vec![url, domain, country]);
             }
         }
     }
-
     out
 }
 
 #[tokio::main(flavor = "multi_thread")]
-async fn check_mirror() -> u8 {
+async fn check_mirror(quiet: bool) -> u8 {
     let args: Args = Args::parse();
     let continous_log: bool = args.continous_log;
+    let no_log = args.no_log;
     let mut tasks = vec![];
     let mirrors = MIRRORS.lock().unwrap();
 
@@ -120,7 +149,7 @@ async fn check_mirror() -> u8 {
         let log_data = Arc::clone(&log_data);
         let url = mirror_data[0].clone();
         let domain = mirror_data[1].clone();
-        //let country = mirror_data[2].clone();
+        let country = mirror_data[2].clone();
         let client = client.clone();
 
         log_data.lock().unwrap().as_object_mut().unwrap().insert(
@@ -132,24 +161,37 @@ async fn check_mirror() -> u8 {
         tasks.push(tokio::spawn(async move {
             let response = client.get(&url).send().await;
             let mut done_mirrors = DONE_MIRRORS.lock().unwrap();
+            let mut response_code = String::from("Error");
+            let mut status_icon= "❌";
             match response {
                 Ok(response) => {
                     if response.status() == StatusCode::OK {
                         log_data.lock().unwrap().as_object_mut().unwrap().get_mut(&url).unwrap()["status"] = json!(1);
                         done_mirrors[0].0 += 1;
+                        status_icon = "✅";
                     } else {
+                        done_mirrors[0].1 += 1;
                         log_data.lock().unwrap().as_object_mut().unwrap().get_mut(&url).unwrap()["status"] = json!(0);
                     }
-                    log_data.lock().unwrap().as_object_mut().unwrap().get_mut(&url).unwrap()["code"] = json!(response.status().as_u16());
+                    response_code = response.status().as_u16().to_string();
+                    log_data.lock().unwrap().as_object_mut().unwrap().get_mut(&url).unwrap()["code"] = json!(response_code);// I'm no Rust dev but i know this is bad, feel free to contrib
+                    log_data.lock().unwrap().as_object_mut().unwrap().get_mut(&url).unwrap()["country"] = json!(country);   // I'm no Rust dev but i know this is bad, feel free to contrib
                 }
                 Err(_) => {
                     log_data.lock().unwrap().as_object_mut().unwrap().get_mut(&url).unwrap()["status"] = json!(0);
                     done_mirrors[0].1 += 1;
                 }
             }
-            print!("\r({} / {} / {})", done_mirrors[0].0, done_mirrors[0].1, done_mirrors[0].2);
+            let percent_suceed: f32 = (done_mirrors[0].0 as f32 / (done_mirrors[0].0 as f32 + done_mirrors[0].1 as f32)) * 100.0;
+            let total_percent: f32 = ((done_mirrors[0].0 as f32 + done_mirrors[0].1 as f32) / done_mirrors[0].2 as f32) * 100.0;
+            let mirrors_left = done_mirrors[0].2 - (done_mirrors[0].0 + done_mirrors[0].1);
+
+            if !quiet {
+                println!("{:<70} {:<30} {} ({})", url, country, status_icon, response_code);
+                print!("[\x1b[31m{}\x1b[0m / \x1b[32m{}\x1b[0m ({:.2}%) / {} / ({}) {:.2}%]\r", done_mirrors[0].1, done_mirrors[0].0, percent_suceed, done_mirrors[0].2, mirrors_left, total_percent);
+            }
             io::stdout().flush().unwrap();
-            if continous_log{
+            if continous_log && !no_log{
                 log(Some(log_data.lock().unwrap().clone()));
             }
         }));
@@ -158,35 +200,45 @@ async fn check_mirror() -> u8 {
     for task in tasks {
         task.await.unwrap();
     }
-    log(Some(log_data.lock().unwrap().clone()));
+    if !no_log{
+        log(Some(log_data.lock().unwrap().clone()));
+    }
     1
 }
 
 
 #[tokio::main]
-async fn get_mirrors(url: &str) -> Result<Vec<Vec<String>>, Error> {
+async fn get_mirrors(url: &str, max_mirrors: isize, country_exclude: Vec<&str>, country_include: Vec<&str>) -> Result<Vec<Vec<String>>, Error> {
     let response = reqwest::get(url).await?.text().await?;
 
-    Ok(parse_mirrors(response))
+    Ok(parse_mirrors(response, max_mirrors, country_exclude, country_include))
 }
 
 fn main() {
     // Reset the log file
     log(None);
 
-    let args: Args = Args::parse();
+    let mut args: Args = Args::parse();
+
+    if args.ask_custom_url {
+        print!("Download URL: [{}]\n# ", args.download_url);
+        args.download_url = read!();
+    }
+
     {
         let mut log_data = LOG_DATA.lock().unwrap();
         log_data["mirror_url"] = Value::String(String::from(&args.download_url));
         log(Some(log_data.clone()));
     }
     {
+        let country_exclude: Vec<&str> = args.exclude_country.split(',').filter(|s| !s.is_empty()).collect();
+        let country_include: Vec<&str> = args.include_country.split(',').filter(|s| !s.is_empty()).collect();
         let mut mirrors = MIRRORS.lock().unwrap();
-        *mirrors = get_mirrors(&args.download_url).unwrap();
+        *mirrors = get_mirrors(&args.download_url, args.max_check, country_exclude, country_include).unwrap();
     }
     {
         let mirrors = MIRRORS.lock().unwrap();
         DONE_MIRRORS.lock().unwrap()[0].2 = mirrors.len();
     }
-    check_mirror();
+    check_mirror(args.quiet);
 }
